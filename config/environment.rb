5d645a08c8670883c242d111bf9c65774b8dce51
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-05 20:03:52 +0000

    finished adding basic files

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ Bundler.require
 require 'active_record'
 require 'rake'
 
-# Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-# Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
-# 
-# DBRegistry[ENV["PLAYLISTER_ENV"]].connect!
-# DB = ActiveRecord::Base.connection
-#
-# if ENV["PLAYLISTER_ENV"] == "test"
-#   ActiveRecord::Migration.verbose = false
-# end
+ Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+ Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+
+ DBRegistry[ENV["STUDENT_ENV"]].connect!
+ DB = ActiveRecord::Base.connection
+
+ if ENV["STUDENT_ENV"] == "test"
+   ActiveRecord::Migration.verbose = false
+ end
